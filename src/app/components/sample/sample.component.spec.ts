c02db04fab9823df4c7bf66f4c444d9ab6be4784
Joel Estumano <joelestumano@gmail.com>
2025-07-03 19:32:11 -0300

    Feat :sparkles: Implementação para cobertura de 100% do relatório de testes.

--- src/app/components/sample/sample.component.spec.ts
+++ src/app/components/sample/sample.component.spec.ts
@@ -1,3 +1,4 @@
+import { By } from '@angular/platform-browser';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SampleComponent } from './sample.component';
 
@@ -11,11 +12,24 @@ describe('SampleComponent', () => {
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(SampleComponent);
+
+		fixture.componentRef.setInput('twClass', 'h-24 w-24');
+
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
 
 	it('deve criar o componente', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('deve renderizar <span class="bg-red-500"> para destacar classes removidas', () => {
+		const p = fixture.debugElement.query(By.css('[data-test-id="high-lighted-removed"]')).nativeElement as HTMLParagraphElement;
+		expect(p.innerHTML).toContain(`<span class="bg-red-500">`);
+	});
+
+	it('deve renderizar <span class="bg-green-500"> para destacar classes sobrescritas', () => {
+		const p = fixture.debugElement.query(By.css('[data-test-id="high-lighted-added"]')).nativeElement as HTMLParagraphElement;
+		expect(p.innerHTML).toContain(`<span class="bg-green-500">`);
 	});
 });
