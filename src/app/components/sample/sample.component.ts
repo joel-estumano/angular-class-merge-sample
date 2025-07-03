dac15a7a5e8e087fe2ca33ef8255b7b8cab74149
Joel Estumano <joelestumano@gmail.com>
2025-07-03 16:23:22 -0300

    Feat :sparkles: Ataualização escopo de acesso.

--- src/app/components/sample/sample.component.ts
+++ src/app/components/sample/sample.component.ts
@@ -7,22 +7,31 @@ import { NgClass } from '@angular/common';
 	selector: 'app-sample',
 	standalone: true,
 	imports: [NgClass],
-	templateUrl: './sample.component.html'
+	templateUrl: './sample.component.html',
 })
 export class SampleComponent {
 	twClass = input<string>();
-	sanitizer = inject(DomSanitizer);
+	private sanitizer = inject(DomSanitizer);
 
-	readonly internalClasses = 'grid place-content-center h-32 w-32 text-foreground font-semibold bg-green-500 rounded';
+	private readonly internalClasses =
+		'grid place-content-center h-32 w-32 text-foreground font-semibold bg-green-500 rounded';
 
-	mergeClassCptd = computed(() => mergeClass(this.internalClasses, this.twClass()));
+	protected mergeClassCptd = computed(() =>
+		mergeClass(this.internalClasses, this.twClass()),
+	);
 
 	highlightedRemovedClasses = computed<SafeHtml>(() => {
 		const original = this.internalClasses.split(/\s+/);
 		const merged = this.mergeClassCptd().split(/\s+/);
 		const removed = original.filter((cls) => !merged.includes(cls));
 
-		const html = original.map((cls) => (removed.includes(cls) ? `<span class="bg-red-500">${cls}</span>` : cls)).join(' ');
+		const html = original
+			.map((cls) =>
+				removed.includes(cls)
+					? `<span class="bg-red-500">${cls}</span>`
+					: cls,
+			)
+			.join(' ');
 
 		return this.sanitizer.bypassSecurityTrustHtml(html);
 	});
@@ -32,7 +41,13 @@ export class SampleComponent {
 		const merged = this.mergeClassCptd().split(/\s+/);
 		const added = merged.filter((cls) => !original.includes(cls));
 
-		const html = merged.map((cls) => (added.includes(cls) ? `<span class="bg-green-500">${cls}</span>` : cls)).join(' ');
+		const html = merged
+			.map((cls) =>
+				added.includes(cls)
+					? `<span class="bg-green-500">${cls}</span>`
+					: cls,
+			)
+			.join(' ');
 
 		return this.sanitizer.bypassSecurityTrustHtml(html);
 	});
